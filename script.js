a9cb786d9371a64fe251ca2b17f37295b57a96d0
Ankit Yadav <115349567+AnkitYadav0809@users.noreply.github.com>
2025-07-22 10:55:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function insert_Row() {
     //Write your code here
-  
+   var table = document.getElementById("sampleTable");
+    
+    // Step 2: Insert a new row at the top
+    var newRow = table.insertRow(0);
+    
+    // Step 3: Insert new cells
+    var cell1 = newRow.insertCell(0);
+    var cell2 = newRow.insertCell(1);
+    
+    // Step 4: Set the cell values
+    cell1.innerHTML = "New Cell1";
+    cell2.innerHTML = "New Cell2";
+}
   
 }
